76e3ae2885f5643dcbf6ebbdbbeebe87848feadb
ProgSomel <somelahmed88@gmail.com>
2024-06-28 18:27:39 +0600

    first commit

---
+++ Chapter 01 - Moudels, Comments, Pip/module.py
@@ -0,0 +1,10 @@
+import pyjokes  # type: ignore
+
+# This will print a Joke 
+joke = pyjokes.get_joke()
+print(joke);
+
+"""
+so thanks 
+that was my program 
+"""
---
+++ Chapter 02 - Variable amd DataType/02_dataTypes.py
@@ -0,0 +1,9 @@
+a = 1 ; #is a integer type variable
+
+b = 5.22 # b is a floating point type number
+
+c = "harry" #c is a string type variable
+
+d = False # d is a boolean type variable
+
+e = None # e is a none type variable
